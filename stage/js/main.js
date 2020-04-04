553c5a5f2151ed537dddc7458be2f9c10917a641
mohamed-abdelkader <mohamed.m90012@gmail.com>
2020-04-04 20:01:02 +0200

    add some edite

--- stage/js/main.js
+++ stage/js/main.js
@@ -1,4 +1,14 @@
-$("body").niceScroll({
-  cursorcolor:"aquamarine",
-  cursorwidth:"15px"
-});
+$(document).ready(function () {
+  'use strict';
+
+  /* Start preloader
+  =====================================*/
+
+    const preloaderDiv = document.querySelector('.preloader');
+    setTimeout(function () {
+      preloaderDiv.className += " fade";
+    }, 5000);
+
+  /* End preloader
+  =====================================*/
+})
