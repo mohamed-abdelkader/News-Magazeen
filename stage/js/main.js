0b9ef49b13ec59b93f2c37ad2d3b48c41b548145
mohamed-abdelkader <mohamed.m90012@gmail.com>
2020-04-24 21:07:08 +0200

    creat new style

--- stage/js/main.js
+++ stage/js/main.js
@@ -2,8 +2,6 @@ $(function(){
 
   /* Start widow prelpader
   -------------------------------------*/
-  // const preloader = document.querySelector('.loader');
-  // console.log(preloader);
   const preloaderDiv = document.querySelector('.loader');
   setTimeout(function () {
     preloaderDiv.className += " faade";
@@ -11,44 +9,58 @@ $(function(){
 
 
   /* Start search box overlay
+  // -------------------------------------*/
+  // const searchBoxOverlay = document.querySelector('.search-box'),
+  //       searchBoxIcon    = document.querySelector('.search-icon'),
+  //       searchBoxContent = document.querySelector('.search_box_content');
+  // searchBoxIcon.onclick = () => {
+  //   searchBoxOverlay.classList.add('open_overlay');
+  // }
+  // searchBoxContent.onclick = () => {
+  //   searchBoxOverlay.classList.remove('open_overlay');
+  // }
+
+  /* Start search box overlay
   -------------------------------------*/
-  const searchBoxOverlay = document.querySelector('.search-box'),
-        searchBoxIcon    = document.querySelector('.search-icon'),
-        searchBoxContent = document.querySelector('.search_box_content');
-  searchBoxIcon.onclick = () => {
-    searchBoxOverlay.classList.add('open_overlay');
-  }
-  searchBoxContent.onclick = () => {
-    searchBoxOverlay.classList.remove('open_overlay');
-  }
 
   /* Start fixed header top
   -------------------------------------*/
-  const headerAreaBottom = document.getElementById('header_area_bottom');
-  window.onscroll = () => {
-    'use strict';
-    if (window.pageYOffset > 150) {
-      headerAreaBottom.classList.add('fixed');
-    } else {
-      headerAreaBottom.classList.remove('fixed')
-    }
-  }
+  // const headerAreaBottom = document.getElementById('header_area_bottom');
+  // window.onscroll = () => {
+  //   'use strict';
+  //   if (window.pageYOffset > 150) {
+  //     headerAreaBottom.classList.add('fixed');
+  //   } else {
+  //     headerAreaBottom.classList.remove('fixed')
+  //   }
+  // }
 /* End fixed header top
 ------------------------------------------*/
 
 /* Start slick nav mobile menu
 --------------------------------------------*/
 
-  $('.nav_links nav').slicknav({
-    label: '',
-    duration: 300,
-    prependTo:'.mobile_menu',
-    closedSymbol: '&#43;',
-    openedSymbol: '&#8722;'
-  });
+  // $('.nav_links nav').slicknav({
+  //   label: '',
+  //   duration: 300,
+  //   prependTo:'.mobile_menu',
+  //   closedSymbol: '&#43;',
+  //   openedSymbol: '&#8722;'
+  // });
 
   /* End slick nav mobile menu
   --------------------------------------------*/
+
+  /* Start banner height
+  -----------------------------------------*/
+
+   $('.banner_slider_img').height($(window).height());
+   $(window).resize(function () {
+     $('.banner_slider_img').height($(window).height());
+   })
+
+  /* End banner height
+  ------------------------------------------*/
 
   /* Start owl Carousel slider
   ---------------------------------------------*/
@@ -56,7 +68,7 @@ $(function(){
     loop:true,
     items:1,
     autoplay:true,
-    navText:['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
+    navText:['<i class="fa fa-long-arrow-left"></i>','<i class="fa fa-long-arrow-right"></i>'],
     nav:true,
     dots:true,
     autoplaySpeed: 500,
@@ -64,10 +76,11 @@ $(function(){
 
   /* End owl Carousel slider
   ---------------------------------------------*/
+
   /* Start treger wow js
   ---------------------------------------------*/
   new WOW().init();
-
   /* End treger wow js
   ---------------------------------------------*/
+
 });
